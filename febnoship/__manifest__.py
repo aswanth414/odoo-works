b1efbc3eefd456eb02834fe10ebe8a25509b8f11
aswanth414 <56402498+aswanth414@users.noreply.github.com>
2021-09-21 12:16:30 +0530

    Add files via upload

---
+++ febnoship/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "febnoship",
+    'summary': """Add Menu to Accounts Module""",
+    'description': """
+           Add Menu to Accounts Module for Adding to new feilds:
+               - Add Menu to Accounts Module
+     """,
+     'author': "",
+     'website': "http://www.yourcompany.com",
+    'category': 'Test',
+    'version': '0.1',
+    'depends': ['sale', 'board'],
+    'data': [
+          'security/security.xml',
+           'security/ir.model.access.csv',
+          'views/ship.xml',
+
+
+
+     ],
+
+}
