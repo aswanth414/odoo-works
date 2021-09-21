b8592746ee7fd5659bdd26740366fb2891d0c8af
aswanth414 <56402498+aswanth414@users.noreply.github.com>
2021-09-21 12:12:58 +0530

    Add files via upload

---
+++ febnoship/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "menu_add",
+    'summary': """Adding menu to a Model""",
+    'description': """
+           Add a Mennu on module for Acoounts Module:
+               - ADDING A MENU
+     """,
+     'author': "",
+     'website': "",
+    'category': 'Test',
+    'version': '0.1',
+    'depends': ['sale', 'board'],
+    'data': [
+          'security/security.xml',
+           'security/ir.model.access.csv',
+           'views/menu.xml',
+
+
+
+     ],
+
+}
